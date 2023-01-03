a0ac636d7cb593d810e2302916cef6a4b65d8b67
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-01-03 12:52:47 +0530

    feat : added rating holder

--- 08 - interactive rating component/script.js
+++ 08 - interactive rating component/script.js
@@ -2,6 +2,7 @@ const messageComp = document.querySelector('#message-component')
 const ratingBtns = document.querySelectorAll('.rating-btn')
 const submitBtn = document.querySelector('#submit-btn')
 const ratingComp = document.querySelector('#rating-component')
+const ratingHolder = document.querySelector('#rating-holder')
 
 
 
@@ -24,6 +25,7 @@ ratingBtns.forEach((e)=>{
 
 const showMessageComp = () => {
     ratingComp.style.display = "none"
-    messageComp.style.display = "block"
+    messageComp.style.display = "flex"
+    ratingHolder.innerText = ratingValue
 }
 submitBtn.addEventListener("click", showMessageComp)
