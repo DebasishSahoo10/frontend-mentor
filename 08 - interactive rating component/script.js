ab2e7dc23af6938da6f08fb3920ba77314387d13
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-01-03 12:34:50 +0530

    feat : added value getting function

--- 08 - interactive rating component/script.js
+++ 08 - interactive rating component/script.js
@@ -1,17 +1,29 @@
 const messageComp = document.querySelector('#message-component')
-
-
-
-
-
+const ratingBtns = document.querySelectorAll('.rating-btn')
+const submitBtn = document.querySelector('#submit-btn')
+const ratingComp = document.querySelector('#rating-component')
 
 
 
 const hideMessage = () => {
     messageComp.style.display = "none"
 }
-
-
-
-
 window.addEventListener("load", hideMessage)
+
+
+let ratingValue = 0
+const getValue = (value) => {
+    ratingValue = value
+    console.log(ratingValue)
+}
+ratingBtns.forEach((e)=>{
+    e.addEventListener("click", ()=>{
+        getValue(e.value)
+    })
+})
+
+const showMessageComp = () => {
+    ratingComp.style.display = "none"
+    messageComp.style.display = "block"
+}
+submitBtn.addEventListener("click", showMessageComp)
