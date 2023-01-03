66a7c1f7b5160e9f91a3cda01c318505c2685365
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-01-03 12:02:11 +0530

    feat : added container hide function

---
+++ 08 - interactive rating component/script.js
@@ -0,0 +1,17 @@
+const messageComp = document.querySelector('#message-component')
+
+
+
+
+
+
+
+
+const hideMessage = () => {
+    messageComp.style.display = "none"
+}
+
+
+
+
+window.addEventListener("load", hideMessage)
